3fa6c91b1dba9de413a22535f2478d713fefecd1
rose-whitt <71530914+rose-whitt@users.noreply.github.com>
2022-09-01 14:51:59 -0500

    out/test files

---
+++ out/production/homework6/test/rice/test/pyfiles/f0multipleMixedReqRandom/student3.py
@@ -0,0 +1,11 @@
+import sys
+
+def func0(intval):
+    if intval in [3, 4]:
+        return intval + 1
+    return intval
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    new_args = [eval(arg) for arg in args]
+    print (func0(*new_args))
